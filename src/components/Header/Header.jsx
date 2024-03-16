b244c2bd3cc0de375763a8df3e65fc72867b9fb6
Alexey Kovalov <alexey.kovalov@gmail.com>
2024-03-16 03:53:58 +0200

    mobile menu added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,35 @@
+import { useState, useEffect } from "react";
 import Logo from "./Logo";
 import Navigation from "./Navigation";
 import Container from "../Shared/Container";
 import css from "./Header.module.scss";
 import useWindowDimensions from "../../hooks/useWindowDimensions";
-import { RxHamburgerMenu } from "react-icons/rx";
+import { RxHamburgerMenu, RxCross2  } from "react-icons/rx";
 
 const Header = () => {
-    const { width } = useWindowDimensions();
+  const [mobileMenuVisible, setMobilMenuVisible] = useState(false);
+  const { width } = useWindowDimensions();
+  const { visible } = css;
+
+  const handleMobileToggle = () => {
+    setMobilMenuVisible(!mobileMenuVisible);
+  };
+
+  const handleNavClick = (event) => {
+    if (event.target.tagName === "A") {
+      handleMobileToggle();
+    }
+  };
+
+  useEffect(() => {
+    if (mobileMenuVisible) {
+      document.body.classList.add("is-open");
+    }
+    if (!mobileMenuVisible) {
+      document.body.classList.remove("is-open");
+    }
+  }, [mobileMenuVisible]);
+
   return (
     <>
       <header className={css.pageHeader}>
@@ -14,8 +37,29 @@ const Header = () => {
           <div className={css.wrapper}>
             <Logo />
             {width > 1440 && <Navigation />}
-            {width < 1440 && <RxHamburgerMenu className={css.burgerIcon}/>}        
+
+          {width < 1440 && (
+              <button
+                className={css.btn}
+                type="button"
+                onClick={handleMobileToggle}
+              >
+                {mobileMenuVisible ? (
+                  <RxCross2 className={css.burgerIcon}/>
+                ) : (
+                    <RxHamburgerMenu className={css.burgerIcon} />
+                )}
+              </button>
+            )}
+          {width < 1440 && (
+          <div
+            className={`${css.mobile} ${mobileMenuVisible ? visible : ''}`}
+            onClick={handleNavClick}
+          >
+            {width < 1440 && <Navigation />}
           </div>
+        )}
+                  </div>
         </Container>
       </header>
     </>
--- src/components/Shared/Layout.jsx
+++ src/components/Shared/Layout.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import css from "./Layout.module.scss";
+// import css from "./Layout.module.scss";
 import Header from "../Header/Header";
 
 const Layout = () => {
