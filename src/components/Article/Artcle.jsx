5d72139b7682b3ac4ae8204f88e7276f815a91bc
Alexey Kovalov <alexey.kovalov@gmail.com>
2024-03-25 09:12:34 +0200

    press cards size & defaul picture changed

--- src/components/Article/Artcle.jsx
+++ src/components/Article/Artcle.jsx
@@ -1,18 +1,28 @@
+import { useState } from "react";
 import css from "./Article.module.scss";
 import { TfiCalendar } from "react-icons/tfi";
+import defaultImage from '../../assets/default-image.png';
+
 
 const Article = ({ articles }) => {
   const { url, title, date, image } = articles;
+  const [imgSrc, setImgSrc] = useState(image);
 
+  const handleError = () => {
+    setImgSrc(defaultImage);
+  }
+
+ 
   return (
     <li className={css.article}>
       <a href={url} target="blank">
-        <img src={image} alt={title} className={css.image} />
+        <img src={imgSrc} alt={title} className={css.image} onError={handleError}/>
         <div className={css.text}>
           <p className={css.date}>
             <TfiCalendar /> {date}
           </p>
           <h2 className={css.title}>{title}</h2>
+          
         </div>
       </a>
     </li>
--- src/pages/PressPage.jsx
+++ src/pages/PressPage.jsx
@@ -1,6 +1,6 @@
 import Container from "../components/Shared/Container";
 import ArticlesSet from "../components/CardSet/CardSet";
-import articles from "../../public/press.json";
+import articles from "../assets/press.json";
 
 const PressPage = () => {
     return (
