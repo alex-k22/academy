c0a2d39bdbae847469cc87a79f7f5d8a60c4e112
Alexey <alexey.kovalov@gmail.com>
2024-09-27 02:42:51 +0300

    team name changed on homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,15 +1,20 @@
 import Container from "components/Shared/Container";
+import PreviousMatch from "../components/PreviousMatch/PreviousMatch";
 import css from "./HomePage.module.scss";
+// import { useEffect, useState } from "react";
 // import StandingTable from "../components/StandingTable/StandingTable";
 
 const HomePage = () => {
+  
   return (
     <section className="section">
       <Container>
         <div className={css.mainImage}>
-            <h1>АФК (ДЮСШ "Юніон")</h1>
+            <h1>АФК (ДЮФК "Юніон")</h1>
         </div>
         {/* <StandingTable /> */}
+        <PreviousMatch />
+        
       </Container>
     </section>
   );
