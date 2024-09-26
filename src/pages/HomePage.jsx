179f1905fd53490c0b45076d00d5108bc64df8b5
Alexey <alexey.kovalov@gmail.com>
2024-09-27 01:33:25 +0300

    homepage img changed

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,13 @@
 import Container from "components/Shared/Container";
 import css from "./HomePage.module.scss";
-import StandingTable from "../components/StandingTable/StandingTable";
+// import StandingTable from "../components/StandingTable/StandingTable";
 
 const HomePage = () => {
   return (
     <section className="section">
       <Container>
         <div className={css.mainImage}>
-            АФК
+            <h1>АФК (ДЮСШ "Юніон")</h1>
         </div>
         {/* <StandingTable /> */}
       </Container>
