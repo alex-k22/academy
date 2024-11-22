f6a1b60f84239716db85388cd56663fdaa5fa491
Alexey <alexey.kovalov@gmail.com>
2024-11-22 09:50:34 +0200

    desktop - result and table side by side

--- src/components/ChampResults/ChampResults.jsx
+++ src/components/ChampResults/ChampResults.jsx
@@ -49,56 +49,63 @@ const ChampResults = () => {
               <img src={dufldoLogo} alt="AFK Logo" className={css.leagueLogo} />
               <h2>Чемпіонат області. 1 ліга</h2>
             </div>
+            <div className={css.tableResults}>
+              <TournamentTable matches={champResults} />
+              {/* <input type="checkbox" className={css.readMoreChecker} id="read-more-checker" /> */}
+              <div className={css.results}>
+                <h3 className={css.subHeader}>Результати матчів</h3>
+                <ul>
+                  {champResults.map((match) => (
+                    <li key={match.id} className={css.match}>
+                      <div className={css.matchWrapper}>
+                        <div className={css.team1}>
+                          <p
+                            className={
+                              match.team1 === "ДЮФК Юніон" ? css.bold : ""
+                            }
+                          >
+                            {match.team1}
+                          </p>
+                          <img
+                            src={getTeamLogo(match.team1)}
+                            alt={match.team1}
+                            className={css.clubLogo}
+                          ></img>
+                        </div>
+                      </div>
+                      <div className={css.score}>
+                        <p>
+                          {match.team1Score}:{match.team2Score}
+                        </p>
+                        <p className={css.date}>
+                          {FormatDate(new Date(match.date))}
+                        </p>
+                      </div>
+                      <div className={css.matchWrapper}>
+                        <div className={css.team2}>
+                          <img
+                            src={getTeamLogo(match.team2)}
+                            alt={match.team2}
+                            className={css.clubLogo}
+                          ></img>
+                          <p
+                            className={
+                              match.team2 === "ДЮФК Юніон" ? css.bold : ""
+                            }
+                          >
+                            {match.team2}
+                          </p>
+                        </div>
+                      </div>
+                    </li>
+                  ))}
+                </ul>
+                {/* <div className={css.bottom}></div> */}
+                {/* <label htmlFor="read-more-checker" className={css.readMoreButton}></label> */}
+              </div>
 
-            <TournamentTable matches={champResults} />
-            <div>
-              <h3 className={css.subHeader}>Результати матчів</h3>
-              <ul>
-                {champResults.map((match) => (
-                  <li key={match.id} className={css.match}>
-                    <div className={css.matchWrapper}>
-                      <div className={css.team1}>
-                        <p
-                          className={
-                            match.team1 === "ДЮФК Юніон" ? css.bold : ""
-                          }
-                        >
-                          {match.team1}
-                        </p>
-                        <img
-                          src={getTeamLogo(match.team1)}
-                          alt={match.team1}
-                          className={css.clubLogo}
-                        ></img>
-                      </div>
-                    </div>
-                    <div className={css.score}>
-                      <p>
-                        {match.team1Score}:{match.team2Score}
-                      </p>
-                      <p className={css.date}>
-                        {FormatDate(new Date(match.date))}
-                      </p>
-                    </div>
-                    <div className={css.matchWrapper}>
-                      <div className={css.team2}>
-                        <img
-                          src={getTeamLogo(match.team2)}
-                          alt={match.team2}
-                          className={css.clubLogo}
-                        ></img>
-                        <p
-                          className={
-                            match.team2 === "ДЮФК Юніон" ? css.bold : ""
-                          }
-                        >
-                          {match.team2}
-                        </p>
-                      </div>
-                    </div>
-                  </li>
-                ))}
-              </ul>
+              {/* <label htmlFor="read-more-checker" className={css.readMoreButton}></label> */}
+              
             </div>
           </div>
         )}
